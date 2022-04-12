f4e303cb046f9f57802c5da952cf9715cfc6cf59
RubeniaLima <rubenia.lima2608@gmail.com>
2022-04-12 08:56:42 -0300

    Tratamento de exceção - delete

--- src/main/java/com/project/orderAPI/entities/Payment.java
+++ src/main/java/com/project/orderAPI/entities/Payment.java
@@ -48,6 +48,7 @@ public class Payment implements Serializable {
     }
 
     public Order getOrder() {
+
         return order;
     }
 
--- src/main/java/com/project/orderAPI/services/UserService.java
+++ src/main/java/com/project/orderAPI/services/UserService.java
@@ -2,8 +2,11 @@ package com.project.orderAPI.services;
 
 import com.project.orderAPI.entities.User;
 import com.project.orderAPI.repositories.UserRepository;
+import com.project.orderAPI.services.exceptions.DatabaseException;
 import com.project.orderAPI.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -28,7 +31,14 @@ public class UserService {
     }
 
      public void delete(Long id){
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+
+        }catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+         }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
      }
 
      public User update(Long id, User obj){
